b65d79249c4b3b05bda3a19358c73e723372b88e
Pawel Sawicki <public@psawicki.de>
2022-10-21 09:00:14 +0200

    Initial commit

---
+++ src/app/book-card/book-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+export type BorderColor = 'red' | 'blue';
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss'],
+})
+export class BookCardComponent implements OnInit {
+  // 1. readonly
+  // 2. depending on the value of _mouseIn
+  get color(): BorderColor {
+    return this._mouseIn ? 'red' : 'blue'; // very important: lightweight O(1)
+  }
+
+  _mouseIn = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  mouseIn(): void {
+    this._mouseIn = true;
+  }
+
+  mouseOut(): void {
+    this._mouseIn = false;
+  }
+}
