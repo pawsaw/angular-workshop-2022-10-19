174d0ca25b737d7f1a4d31447de4823e0b042b31
Pawel Sawicki <public@psawicki.de>
2022-10-24 12:28:12 +0200

    HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { BookModule } from './book/book.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BookModule],
+  imports: [BrowserModule, AppRoutingModule, BookModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/book/book-api.service.ts
+++ src/app/book/book-api.service.ts
@@ -1,15 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { Book } from './book';
-import { books } from './data/books';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookApiService {
-  constructor() {}
+  private readonly RESOURCE = 'books';
+
+  constructor(private readonly _http: HttpClient) {}
 
   all(): Observable<Book[]> {
-    return of(books);
+    return this._http.get<Book[]>(`${environment.baseUrl}/${this.RESOURCE}`);
   }
 }
